93e32dd9e92da7bb73f3948c5b037ae0b98aa171
Bartosz Laufer <bartosz.laufer@comarch.pl>
2024-02-06 13:46:15 +0100

    small fix

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # aoc-2023
 
 Advent of Code 2023
-[template]: https://github.com/kotlin-hands-on/advent-of-code-kotlin-template
--- src/day02/Day02.kt
+++ src/day02/Day02.kt
@@ -7,12 +7,9 @@ data class Game(val id: Int, val red: Int, val green: Int, val blue: Int) {
 private fun parseInput(input: String): Game {
   val id = input.substringBefore(":").substringAfter(" ").toInt()
   val colors = mutableMapOf<String, Int>()
-  input.substringAfter(": ").split("; ").forEach { turn ->
-    turn.split(", ").forEach { draw ->
-      val count = draw.substringBefore(" ").toInt()
-      val color = draw.substringAfter(" ")
-      colors[color] = maxOf(count, colors[color] ?: count)
-    }
+  input.substringAfter(": ").split("; ", ", ").forEach { draw ->
+    val (count, color) = draw.split(" ")
+    colors[color] = maxOf(count.toInt(), colors[color] ?: count.toInt())
   }
   return Game(id, colors["red"] ?: 0, colors["green"] ?: 0, colors["blue"] ?: 0)
 }
@@ -21,10 +18,6 @@ fun main() {
   fun part1(input: List<Game>) = input.sumOf { it.isPossible() }
 
   fun part2(input: List<Game>) = input.sumOf { it.multiply() }
-
-  val testInput = readInput("day02/test").map { parseInput(it) }
-  part1(testInput).println()
-  part2(testInput).println()
 
   val input = readInput("day02/input").map { parseInput(it) }
   part1(input).println()
